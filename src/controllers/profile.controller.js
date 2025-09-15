90863b15178a7b20399c0aba3065cd13ef0807ae
Erika <erica.mellao2005@gmail.com>
2025-09-15 20:56:10 -0300

    Se agregaron las funciones para modificar y eliminar un perfil de usuario

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,4 +1,7 @@
-import { handleSuccess } from "../Handlers/responseHandlers.js";
+import { handleSuccess, handleErrorClient, handleErrorServer } from "../Handlers/responseHandlers.js";
+import { User } from "../entities/user.entity.js";
+import { AppDataSource } from "../config/configDb.js";
+import bcrypt from "bcrypt";
 
 export function getPublicProfile(req, res) {
   handleSuccess(res, 200, "Perfil público obtenido exitosamente", {
@@ -13,4 +16,52 @@ export function getPrivateProfile(req, res) {
     message: `¡Hola, ${user.email}! Este es tu perfil privado. Solo tú puedes verlo.`,
     userData: user,
   });
+}
+
+export async function updatePrivateProfile(req, res) {
+  try {
+    const userRepository = AppDataSource.getRepository(User);
+
+    const userId = req.user.sub;
+    const { email, password } = req.body;
+
+    const user = await userRepository.findOne({ where: { id: userId } });
+    if (!user) return handleErrorClient(res, 404, "Usuario no encontrado");
+
+    if (email) user.email = email;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    await userRepository.save(user);
+
+    handleSuccess(res, 200, "Perfil actualizado exitosamente", {
+      message: `Tu perfil ha sido actualizado.`,
+      userData: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    handleErrorServer(res, 500, "Error al actualizar perfil", error);
+  }
+}
+export async function deletePrivateProfile(req, res){
+  try{
+    const userRepository = AppDataSource.getRepository(User);
+
+    const userId = req.user.sub;
+    const user = await userRepository.findOne ({where: {id: userId}});
+    
+    if(!user) return handleErrorClient(res, 404, "Perfil no encontrado");
+
+    await userRepository.remove(user);
+
+    handleSuccess(res, 200, "Perfil eliminado exitosamente", {message: "Tu perfil ha sido eliminado correctamente"});
+
+  }catch(error){
+    handleErrorServer(res, 500, "Error al eliminar perfil", error);
+  }
 }
